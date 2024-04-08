8ac72230c6849d966b28c060861e0514fa7a4dc6
dFarras <diegofarrasguevara@gmail.com>
2024-04-08 03:31:41 +0200

    Add integration tests

--- src/test/java/com/playtomic/tests/wallet/service/impl/StripeServiceIT.java
+++ src/test/java/com/playtomic/tests/wallet/service/impl/StripeServiceIT.java
@@ -5,15 +5,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.playtomic.tests.wallet.configuration.StripeConfiguration;
 import com.playtomic.tests.wallet.exceptions.HttpException;
-import com.playtomic.tests.wallet.service.*;
-
+import com.playtomic.tests.wallet.service.Payment;
+import com.playtomic.tests.wallet.service.StripeRestTemplateResponseErrorHandler;
+import com.playtomic.tests.wallet.service.StripeService;
+import com.playtomic.tests.wallet.service.StripeServiceException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,14 +22,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.test.web.client.RequestMatcher;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.net.URI;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.matches;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
@@ -46,7 +44,7 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
         StripeConfiguration.class,
         StripeRestTemplateResponseErrorHandler.class})
 @RestClientTest
-public class StripeServiceTest {
+public class StripeServiceIT {
     private static final URI TEST_URI = URI.create("https://sandbox.playtomic.io/v1/stripe-simulator/charges");
     private static final String EXPECTED_CODE = "INVALID-002";
     private static final String EXPECTED_MESSAGE = "Payment gateway only accepts charges over 10€.";
