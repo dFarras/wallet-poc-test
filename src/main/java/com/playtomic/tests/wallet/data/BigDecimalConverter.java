9c5351bc9a287b51e28101dda36b7c5ff51299cf
dFarras <diegofarrasguevara@gmail.com>
2024-04-08 14:53:48 +0200

    Add NotNull to BigDecimalConverter

--- src/main/java/com/playtomic/tests/wallet/data/BigDecimalConverter.java
+++ src/main/java/com/playtomic/tests/wallet/data/BigDecimalConverter.java
@@ -2,16 +2,19 @@ package com.playtomic.tests.wallet.data;
 
 import com.playtomic.tests.wallet.exceptions.ErrorCatalog;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 
 @Component
+@Validated
 public class BigDecimalConverter {
     private static final MathContext MATH_CONTEXT = new MathContext(10, RoundingMode.HALF_UP);
 
-    public BigDecimal from(String value) {
+    public BigDecimal from(@NotNull String value) {
         try {
             return new BigDecimal(value, MATH_CONTEXT);
         } catch (NumberFormatException e) {
@@ -19,7 +22,7 @@ public class BigDecimalConverter {
         }
     }
 
-    public String from(BigDecimal result) {
+    public String from(@NotNull BigDecimal result) {
         return result.toPlainString();
     }
 }
